be7224a0c5fc2fb23f3d323be879dd5a82935966
Justin <jribarich98@gmail.com>
2023-01-18 13:19:15 -0500

    BEgan making beginning stuff

---
+++ client/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+}
+
+function Hello() {
+  return (
+    <div>
+      <h1>helllllllloooo</h1>
+    </div>
+  );
+}
+
+export default App;
